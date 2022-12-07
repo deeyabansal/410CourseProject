c75b346d630d112ab59ff5a3fd91e6f38178a27d
isha-akella <iakella2@illinois.edu>
2022-12-06 18:27:22 -0600

    Ranking and terminal prompt complete, comments added

--- parse.py
+++ parse.py
@@ -1,5 +1,4 @@
 import os
-# import io
 import sys
 import metapy
 import nltk
@@ -8,28 +7,43 @@ nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 # File responsible for parsing through lectures, removing unnecessary words (stop words), and returning txt file of relevant words
 
+# Iterates through all lectures and removes the timings present in the transcript 
+# Input - file to iterate through
+# Output = new file without timings 
 def delete_timings(filename):
-    file = open(filename,'r')
+    file = open(filename, 'r')
+    # Creating new file name 
     new_file_name = "Parsed_" + filename[:len(filename)- 4] + "_parsed.txt"
+    # If file does not already exist, create one 
     os.makedirs(os.path.dirname(new_file_name), exist_ok=True)
     f = open(new_file_name, "w")
+    # Removes every other line - contains the timings 
     file_lines = file.readlines()
     for i in range(0, len(file_lines), 2):
         f.write(file_lines[i])
 
+# Iterates through all timing removed lectures and copies the non stopwords
+# Input - file to iterate through
+# Output = new file without all stop words
 def delete_stop_words(filename):
     file = open(filename) 
+    # Creating new file name 
     new_file_name = "SWRemoved_" +filename[7:len(filename)-10] + "removed.txt"
+    # Obtaining stop_words list from existing library and extending it for our project 
     stop_words = list(stopwords.words('english')) 
     new_stopwords = ["in", "lecture", "we", "today", "And", "'re","In", "But", "continue", "today's", "this", "going", "us", "We", "but", "would",
      "like", "This", "we're", "so", "So", "?", ",", "'s", "'nt", "'ll","n't", "'ve", "'d", " [ MUSIC ]", "[ INAUDIBLE ]", "I", "if", "If", "goes", 
      "A", "it", "It", "now", "Now", "these", "The", "do", "These", "see", "You", "you", "also", "Also"]
     stop_words.extend(new_stopwords)
     line = file.read()
+    # Tokenizes the extended stop words
     extended = word_tokenize(line)
+    # If file doesn't already exist, create it 
+    os.makedirs(os.path.dirname(new_file_name), exist_ok=True)
     f = open(new_file_name, "w")
     f.write("")
     f.close()
+    # Iterates through file and appends word to new file if not a stop word
     for r in extended: 
         if not r in stop_words: 
             rewriteFile = open(new_file_name,"a") 
@@ -40,18 +54,26 @@ def delete_stop_words(filename):
             rewriteFile.close()
 
 
+# Removes the stop words from the MP files
+# Input - file to iterate through
+# Output = new file without all stop words
 def delete_mp_stop_words(filename):
     file = open(filename)
+    # Creating new file name
     new_file_name = "SWRemoved_" +filename[:len(filename)-4] + "_removed.txt"
+    # Obtaining stop_words list from existing library and extending it for our project 
     stop_words = list(stopwords.words('english')) 
     new_stopwords = ["do", "your job is to", "must", "submit", "pip install", "(", ")", "Coursera", ",", ":", "`", "``", "!", "mac", "Python", 
     "python", " = ", "'s", "'re", "'nt", "pip install", "To avoid issues please"]
     stop_words.extend(new_stopwords)
     line = file.read()
     extended = word_tokenize(line)
+    # If file doesn't already exist, create it
+    os.makedirs(os.path.dirname(new_file_name), exist_ok=True)
     f = open(new_file_name, "w")
     f.write("")
     f.close()
+    # Iterates through file and appends word to new file if not a stop word
     for r in extended: 
         if not r in stop_words: 
             rewriteFile = open(new_file_name,"a") 
@@ -62,53 +84,57 @@ def delete_mp_stop_words(filename):
             rewriteFile.close()
 
 
+# Calls delete_timings for all imported lectures
+# Input
+#   weeks: array of lecture week numbers
+#   end: number of lectures in each week
+def remove_lecture_timings(weeks, end):
+    for j in weeks:
+        for i in range(1, end):
+            delete_timings("Lectures/Week_" + j + "/" + j + "_" + i + ".txt")
+
+# Calls delete_stop_words for all imported lectures
+# Input
+#   weeks: array of lecture week numbers
+#   end: number of lectures in each week
+def remove_lecture_stopwords(weeks, end):
+    for j in weeks:
+        for i in range(1, end):
+            delete_stop_words("Parsed_Lectures/Week_" + j + "/" + j + "_" + i + "_parsed.txt")
+
+# Calls delete_mp_stop_words for all imported MP ReadMes
+# Input
+#   mps: array of MP numbers
+def remove_mp_stopwords(mps):
+    for i in mps:
+        delete_mp_stop_words("MPs/MP" + i + ".txt")
+
 
 if __name__ == '__main__':
+    # organized by weeks that have the same amount of lectures
+    sevens = ["1", "2", "3"]
+    eights = ["4", "11", "12"]
+    nines = ["5"]
+    tens = ["7", "10"]
+    elevens = ["6", "8", "9"]
+    
     print("Removing timings of Lectures")
-    sevens = [1, 2, 3]
-    for j in sevens :
-        for i in range(1, 7):
-            delete_timings("Lectures/Week_" + str(j) + "/" + str(j) + "_" + str(i) + ".txt")
-    for i in range(1, 8):
-        delete_timings("Lectures/Week_4/4_" + str(i) + ".txt")
-        delete_timings("Lectures/Week_11/11_" + str(i) + ".txt")
-        delete_timings("Lectures/Week_12/12_" + str(i) + ".txt")
-
-    for i in range(1, 9):
-        delete_timings("Lectures/Week_5/5_" + str(i) + ".txt")
-    for i in range(1, 10):
-        delete_timings("Lectures/Week_7/7_" + str(i) + ".txt")
-        delete_timings("Lectures/Week_10/10_" + str(i) + ".txt")
-    for i in range(1, 11):
-        delete_timings("Lectures/Week_6/6_" + str(i) + ".txt")
-        delete_timings("Lectures/Week_8/8_" + str(i) + ".txt")
-        delete_timings("Lectures/Week_9/9_" + str(i) + ".txt")
+    remove_lecture_timings(sevens, 7)
+    remove_lecture_timings(eights, 8)
+    remove_lecture_timings(nines, 9)
+    remove_lecture_timings(tens, 10)
+    remove_lecture_timings(elevens, 11)
 
     print("Removing Stop Words of Lectures")
-    sevens = [1, 2, 3]
-    for j in sevens :
-        for i in range(1, 7):
-            delete_stop_words("Parsed_Lectures/Week_" + str(j) + "/" + str(j) + "_" + str(i) + "_parsed.txt")
-    for i in range(1, 8):
-        delete_stop_words("Parsed_Lectures/Week_4/4_" + str(i) + "_parsed.txt")
-        delete_stop_words("Parsed_Lectures/Week_11/11_" + str(i) + "_parsed.txt")
-        delete_stop_words("Parsed_Lectures/Week_12/12_" + str(i) + "_parsed.txt")
-    for i in range(1, 10):
-        delete_stop_words("Parsed_Lectures/Week_7/7_" + str(i) + "_parsed.txt")
-        delete_stop_words("Parsed_Lectures/Week_10/10_" + str(i) + "_parsed.txt")
-    for i in range(1, 9):
-        delete_stop_words("Parsed_Lectures/Week_5/5_" + str(i) + "_parsed.txt")
-    for i in range(1, 11):
-        delete_stop_words("Parsed_Lectures/Week_6/6_" + str(i) + "_parsed.txt")
-        delete_stop_words("Parsed_Lectures/Week_8/8_" + str(i) + "_parsed.txt")
-        delete_stop_words("Parsed_Lectures/Week_9/9_" + str(i) + "_parsed.txt")
+    remove_lecture_stopwords(sevens, 7)
+    remove_lecture_stopwords(eights, 8)
+    remove_lecture_stopwords(nines, 9)
+    remove_lecture_stopwords(tens, 10)
+    remove_lecture_stopwords(elevens, 11)
 
 
     print("Removing Stop Words from MPs")
-    mps = [1, 3, 4]
-    for i in mps:
-        delete_mp_stop_words("MPs/MP" + str(i) + ".txt")
-    for i in range(1, 5):
-        delete_mp_stop_words("MPs/MP2_" + str(i) + ".txt")
+    mps = ["1", "2_1", "2_2", "2_3", "2_4", "2_5", "3", "4"]
+    remove_mp_stopwords(mps)
 
     
--- rank.py
+++ rank.py
@@ -2,32 +2,17 @@ from rank_bm25 import BM25Okapi
 import metapy
 import pandas as pd
 from rank_bm25 import *
+import math
 import os
 
+# Takes in a mp_number and returns the relevant lectures within the weeks that the MP was 'live' in relevance order
 def ranking(mp_num):
-    if mp_num == '1':
-        start = 1
-        end = 2
-    if mp_num == '2_1':
-        start = 3
-        end = 3
-    if mp_num == '2_2':
-        start = 4
-        end = 4
-    if mp_num == '2_3':
-        start = 5
-        end = 5
-    if mp_num == '2_4':
-        start = 6
-        end = 6
-    if mp_num == '3':
-        start = 7
-        end = 7
-    if mp_num == '4':
-        start = 8
-        end = 12
+    # Get the lecture bounds based on which MP user requests  
+    start = mp_start_lecture_bounds(mp_num)
+    end = mp_end_lecture_bounds(mp_num)
 
     corpus = []
+    # For all weeks, check the number of lectures preset
     for x in range(start, end + 1):
         dir_path = 'SWRemoved_Lectures/Week_'+ str(x)
         lecture_count = 0
@@ -35,7 +20,9 @@ def ranking(mp_num):
         for path in os.listdir(dir_path):
             # check if current path is a file
             if os.path.isfile(os.path.join(dir_path, path)):
+                # increment lecture count
                 lecture_count += 1
+        # within each week, remove new lines and add to corpus        
         for y in range(1, lecture_count + 1):
             f = dir_path + '/' + str(x) + '_' + str(y) + '_removed.txt'
             with open(f, 'r') as file:
@@ -43,47 +30,64 @@ def ranking(mp_num):
                 file.close
                 corpus.append(corpus1)
      
-          
+    # split every doc in the corpus by spaces and then run BM25 on the corpus
     tokenized_corpus = [doc.split(" ") for doc in corpus]
     bm25 = BM25Okapi(tokenized_corpus)
+    
+    # opening every lecture file, removing new lines, and splitting it by spaces
     mp_file = 'SWRemoved_MPs/MP' + str(mp_num) + '_removed.txt'
-
     with open(mp_file, 'r') as file:
         query = file.read().replace('\n', '')
-
     tokenized_query = query.split(" ")
+
+    # obtaining the relevance scores for the lecures
     doc_scores = bm25.get_scores(tokenized_query)
+    # ordering them by highest number (most relevance) to lowest number (least relevance)
     ordered_doc_scores = sorted(doc_scores, reverse=True)
 
-  
+    # Call sort_order and sorted_pairs to match the lectures to their rankings and then print them in order of relevance 
     order = sort_order(doc_scores, ordered_doc_scores)
     pairs = sorted_pairs(order, start, end)
-    return print_lectures(pairs)
     file.close
+    print(print_lectures(pairs))
+    return print_lectures(pairs)
 
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+# Iterates through the document scores and ordered document scores to match the indices and returns the number order of lecture relevance 
 def sort_order(doc_scores, ordered_doc_scores):
     num_order = []
     for i in range(len(ordered_doc_scores)):
         for j in range(len(doc_scores)):
+            # Once match is found between original and ordered scores, append the index number of original document score to list 
             if ordered_doc_scores[i] == doc_scores[j]:
                 num_order.append(j)
+    # Add 1 to each number in list to eliminate 0
     for i in range(len(num_order)):
         num_order[i] += 1
     return num_order
 
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+# Returns relevance number sorted tuples
 def sorted_pairs(order, start, end):
     pairs = []
     begin = 0
+    # Iterates through each relevant week and counts the number of lectures present in that week
     for week in range(start, end+1):
         dir_path = 'SWRemoved_Lectures/Week_'+ str(week)
         lecture_count = 0
-        
         for path in os.listdir(dir_path):
             if os.path.isfile(os.path.join(dir_path, path)):
                 lecture_count += 1
+        
+        # Appends the lecture number to each number in 'order' parameter and returns a sorted pair of tuples
+        # First value in tuple is the relevance ranking relative to the other documents
+        # Second value in tuple is the Lecture number
         curr = 1
         for x in range(begin, begin + lecture_count):
             pairs.append((order[x], "Lecture " + str(week) + "_" + str(curr)))
@@ -93,15 +97,21 @@ def sorted_pairs(order, start, end):
     return pairs
     
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+# Returns the relevant lectures in order 
 def print_lectures(pairs):
     result = [tup[1] for tup in pairs]
-    # for i in result:
-    #     print(i)
     return result
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 # Create lecture week boudaries for each MP
 # returns max week an MP could reference
-def mp_lecture_bounds(mp_num):
+def mp_end_lecture_bounds(mp_num):
     if mp_num == "1":
         return 2
     elif mp_num == "2_1":
@@ -113,11 +123,38 @@ def mp_lecture_bounds(mp_num):
     elif mp_num == "2_4":
         return 6
     elif mp_num == "3":
-        return 7
+        return 9
     elif mp_num == "4":
         return 12
-        
 
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
+# returns start week of previous MP
+def mp_start_lecture_bounds(mp_num):
+    if mp_num == "1":
+        return 1
+    elif mp_num == "2_1":
+        return 3
+    elif mp_num == "2_2":
+        return 4
+    elif mp_num == "2_3":
+        return 5
+    elif mp_num == "2_4":
+        return 6
+    elif mp_num == "3":
+        return 7
+    elif mp_num == "4":
+        return 8     
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
+# Checks if mp number provided to terminal prompt is an existing MP
 def check_valid_input(inp):
     if len(inp) == 1 and (inp == "1" or inp == "3" or inp == "4"):
         return True
@@ -127,37 +164,42 @@ def check_valid_input(inp):
     return False
 
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
+
 if __name__ == '__main__':
     print("---------------------------------------------------------------------------------------------------------------------------------")
-    #ranking('4')
+    ranking('3')
 
-    yo = []
-    end = len(yo)
-    while True:
-        var = input("Enter MP: ")
-        # check if they want to exit
-        if var.lower() == "no":
-            print("Good luck!")
-            break
+    # yo = []
+    # end = math.inf
+    # while True:
+    #     var = input("Enter MP: ")
+    #     # check if they want to exit
+    #     if var.lower() == "done":
+    #         print("Good luck!")
+    #         break
         
-        # # change how many relevant lectures are shown
-        # if var.lower() == "help":
-        #     uh = input("How many lectures would you like to see? ")
-        #     if int(uh) < len(yo):
-        #         end = int(uh)
-        #     else:
-        #         end = len(yo)
-        #     continue
+    #     # change how many relevant lectures are shown
+    #     if var.lower() == "number":
+    #         uh = input("How many lectures would you like to see? ")
+    #         end = int(uh)
+    #         continue
         
-        # check if valid MP
-        if check_valid_input(var) == False:
-            print("That MP does not exist.")
-            continue
+    #     # check if valid MP
+    #     if check_valid_input(var) == False:
+    #         print("That MP does not exist.")
+    #         continue
 
-        # print out relevant lecture
-        print("The most relevant lectures are: ")
-        # call ranking function and print names
-        yo = ranking(var)
-        end = len(yo)
-        for i in range(0, end):
-            print(yo[i])
+    #     # print out relevant lecture
+    #     print("The most relevant lectures are: ")
+    #     # call ranking function and print names
+    #     yo = ranking(var)
+    #     if end > len(yo):
+    #         last = len(yo)
+    #     else:
+    #         last = end
+    #     for i in range(0, last):
+    #         print(yo[i])
